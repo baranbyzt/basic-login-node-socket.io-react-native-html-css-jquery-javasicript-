abb51966fb2315496419fc39f025315a9b5680b6
baranbyzt <87334718+baranbyzt@users.noreply.github.com>
2021-10-13 11:47:14 +0300

    Update client.js

--- client_1/client.js
+++ client_1/client.js
@@ -1,23 +1,23 @@
 
-$(document).ready(function(){
+    $(document).ready(function(){
 
-const socket = io.connect('http://localhost:3000/');
+    const socket = io.connect('http://localhost:3000/');
 
-    $("button").click(function(){
-        var email = $(".email_input").val();
-        var password = $(".pasworld_input").val();
+        $("button").click(function(){
+            var email = $(".email_input").val();
+            var password = $(".pasworld_input").val();
 
 
-        socket.emit('Login_information', {
-          email:email,
-          password:password
+            socket.emit('Login_information', {
+            email:email,
+            password:password
+            });
+
+
+
+    
+    
         });
-
-
-
-  
-   
-    });
 
  socket.on('passwordcorrect', (data) => {
      
